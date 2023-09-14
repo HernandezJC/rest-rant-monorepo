b067046bdbef3e0b130eb9b0f77700be70efe2f9
HernandezJC <jchernandez1021@gmail.com>
2023-09-14 04:32:25 -0700

    admin account

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -8,6 +8,7 @@ router.post('/', async (req, res) => {
     let { password, ...rest } = req.body;
     const user = await User.create({
         ...rest,
+        role: 'reviewer',
         passwordDigest: await bcrypt.hash(password, 10)
     })
     res.json(user)
@@ -20,3 +21,4 @@ router.get('/', async (req, res) => {
 })
 
 module.exports = router
+
